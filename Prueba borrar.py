2ba50905d5d640204f8bfc3e89ef20b8d9ca8cc9
Diego Guzmán <124654195+daguzman545@users.noreply.github.com>
2023-05-23 15:39:09 -0500

    avances varios

--- Prueba borrar.py
+++ Prueba borrar.py
@@ -4,13 +4,15 @@ import json
 from urllib.request import urlopen
 
 """## Importar json con las ubicaciones por departamento"""
-with urlopen('https://github.com/finiterank/mapa-colombia-js/blob/9ae3e4e6125e2589cc1ac8bf685ebb319d99cf08/colombia-municipios.json') as response:
+with urlopen('https://raw.githubusercontent.com/finiterank/mapa-colombia-js/9ae3e4e6125e2589cc1ac8bf685ebb319d99cf08/colombia-municipios.json') as response:
     counties = json.load(response)
+# Ojo el link al repositorio con el json falla luego de un tiempo, creo que no estoy generando bien el perma link
+df = pd.read_csv('datos4.csv', delimiter=',')
 
-df = pd.read_csv('datos2.csv', header=None, delimiter=',')
 
-locs = df['cole_depto_ubicacion']
 
-for loc in counties['Topology']:
+locs = df['cole_mcpio_ubicacion']
+
+for loc in counties['arcs']:
     loc['id'] = loc['properties']['name']
 
