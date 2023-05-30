f697d15d614a58f2f752a402825e2a3807346916
Diego Guzmán <124654195+daguzman545@users.noreply.github.com>
2023-05-30 17:16:36 -0500

    act

--- Prueba borrar.py
+++ Prueba borrar.py
@@ -1,18 +1,18 @@
-import pandas as pd
-import plotly.graph_objs as go
+import plotly.graph_objects as go
 import json
 from urllib.request import urlopen
-
-"""## Importar json con las ubicaciones por departamento"""
-with urlopen('https://raw.githubusercontent.com/finiterank/mapa-colombia-js/9ae3e4e6125e2589cc1ac8bf685ebb319d99cf08/colombia-municipios.json') as response:
+with urlopen('https://gist.githubusercontent.com/john-guerra/43c7656821069d00dcbc/raw/be6a6e239cd5b5b803c6e7c2ec405b793a9064dd/Colombia.geo.json') as response:
     counties = json.load(response)
-# Ojo el link al repositorio con el json falla luego de un tiempo, creo que no estoy generando bien el perma link
-df = pd.read_csv('datos4.csv', delimiter=',')
 
-
-
-locs = df['cole_mcpio_ubicacion']
-
-for loc in counties['arcs']:
-    loc['id'] = loc['properties']['name']
-
+for loc in counties['objects']["depts"]["geometries"]:
+    print(loc["properties"]["dpt"])  
+fig = go.Figure(go.Choroplethmapbox(
+                    geojson=counties,
+                    locations=locs,
+                    z=[1, 2, 3],
+                    colorscale='Viridis',
+                    colorbar_title="Thousands USD"))
+fig.update_layout(mapbox_style="carto-positron",
+                        mapbox_zoom=3,
+                        mapbox_center = {"lat": 4.570868, "lon": -74.2973328})
+fig.show()
